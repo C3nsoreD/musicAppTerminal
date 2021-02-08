6ff7482b43b1b06e6acb3132df69f7af3b386ab9
c3nsored <ur.gabriel@outlook.com>
2021-02-08 23:38:46 +0100

    core models request and request_type

--- musicapp/pytify/core/request.py
+++ musicapp/pytify/core/request.py
@@ -7,4 +7,33 @@ from .request_type import RequestType
 
 
 def execute_request(url_template, auth, params, request_type=RequestType.GET, payload=()):
-    
+    """
+    `url_template`: the template used to build the url to perform requests
+    'auth': authentication object
+    'params': Params paced needed for url
+    'request': GET or PUT
+    'payload': any data that may be sent.
+    """
+    conf = read_config()
+    params['base_url'] = conf.base_url
+    url = url_template.format(**params)
+
+    headers = {
+        'Authorization': f"Bearer {auth.access_token}"
+    }
+
+    if request_type is RequestType.GET:
+        response = requests.get(url, headers=headers)
+    else:
+        response = requests.put(url, headers=headers, data=json.dumps(payload))
+        # check it there is response 'not empty'
+        if not response.text:
+            return response.text
+
+    result = json.loads(response.text)
+
+    if not response.ok:
+        error = result['error']
+        raise BadRequestError(
+            f"{error['message']} (HTTP {error['status']})"
+        )
